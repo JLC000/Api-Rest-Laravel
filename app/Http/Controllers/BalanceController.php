5ee78cf2b5799af38d524296d548e1547b63ff39
JLC000 <joseluiscruz1914@gmail.com>
2021-05-24 22:49:40 -0300

    Add event endpoint to create an account with an initial balance

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Account;
 
 class BalanceController extends Controller
 {
-    //
+    public function show(Request $request){
+
+        $accountId = $request->input('account_id');
+
+        $account = Account::findOrFail($accountId);
+
+        return $account->balance;
+    }
+
+
 }
--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Account;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function store(Request $request){
+
+        if($request->input('type') === 'deposit'){
+           return $this->deposit($request->input('destination'),$request->input('amount'));
+        }
+    }
+
+    private function deposit($destination, $amount){
+        $account = Account::firstOrCreate(['id' => $destination] ); //condition search
+
+        $account->balance += $amount;
+        
+        $account->save();   //update
+
+        return response()->json([
+            'destination' => [
+                'id' => $account->id,
+                'balance' => $account->balance,
+            ],
+
+        ], 201);
+    }
 }
